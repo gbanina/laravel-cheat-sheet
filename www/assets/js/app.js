b352b5f0619b40eecca96395739a509ebc2cb42d
Goran Banina <gbanina@gmail.com>
2014-03-01 16:35:18 +0100

    test

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -6,9 +6,6 @@ $('.comments-toggle').on('click', function(event){
 // Call prettprint manually
 prettyPrint();
 
-// For ajax calls + randomize to clear cache
-$(document).ready(function(){ alert(); prettyPrint(); })
-
 // Shine up the HTML:: calls
 var $html_elements = $("span.pln:contains('HTML')").removeClass('pln').addClass('typ');
 var $url_elements = $("span.pln:contains('URL')").removeClass('pln').addClass('typ');
